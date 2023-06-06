867292c4130d987eb7121958da9128d8652039d8
Joshua Johns <joshuajohns28@outlook.com>
2023-06-05 20:02:38 -0500

    added cake controller

---
+++ Controllers/CakeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JandKBakery.Controllers
+{
+    public class CakeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
